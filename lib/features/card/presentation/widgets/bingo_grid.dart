7f85c10142b0a61db90dea5f7c84a4a1ec6413a0
S.k2004 <stormkroonen@hotmail.nl>
2026-04-23 00:42:29 +0200

    users cant uncheck the free square

--- lib/features/card/presentation/widgets/bingo_grid.dart
+++ lib/features/card/presentation/widgets/bingo_grid.dart
@@ -10,10 +10,7 @@ import 'bingo_cell_widget.dart';
 class BingoGrid extends ConsumerWidget {
   final List<CellModel> cells;
 
-  const BingoGrid({
-    super.key,
-    required this.cells,
-  });
+  const BingoGrid({super.key, required this.cells});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -30,14 +27,19 @@ class BingoGrid extends ConsumerWidget {
       itemCount: cells.length,
       itemBuilder: (context, index) {
         // Sort cells by position if the backend doesn't guarantee order
-        final sortedCells = [...cells]..sort((a, b) => a.position.compareTo(b.position));
+        final sortedCells = [...cells]
+          ..sort((a, b) => a.position.compareTo(b.position));
         final cell = sortedCells[index];
 
         return BingoCellWidget(
           cell: cell,
           onTap: () {
             // Use the notifier to send the update to NestJS
-            ref.read(currentMatchProvider.notifier).toggleCellCheck(cell.id, !cell.isChecked);
+            if (cell.value != "FREE") {
+              ref
+                  .read(currentMatchProvider.notifier)
+                  .toggleCellCheck(cell.id, !cell.isChecked);
+            }
           },
         );
       },
